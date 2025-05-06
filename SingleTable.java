2a916369b4a3bec878023757df540a29280ed6e9
Ashley <167535539+hello135790@users.noreply.github.com>
2025-05-06 03:07:55 +0000

    Starter code

---
+++ SingleTable.java
@@ -0,0 +1,25 @@
+public class SingleTable {
+    /**
+     * Returns the number of seats at this table. The value is always greater than
+     * or equal to 4.
+     */
+    public int getNumSeats() {
+        /* implementation not shown */ }
+
+
+    /** Returns the height of this table in centimeters. */
+    public int getHeight() {
+        /* implementation not shown */ }
+
+
+    /** Returns the quality of the view from this table. */
+    public double getViewQuality() {
+        /* implementation not shown */ }
+
+
+    /** Sets the quality of the view from this table to value . */
+    public void setViewQuality(double value) {
+        /* implementation not shown */ }
+    // There may be instance variables, constructors, and methods that are not
+    // shown.
+}
